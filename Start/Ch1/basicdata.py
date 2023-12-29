df8d92c83e6b6112c355180ad378dc13192eaa2c
Prahalad Srinivas C G <32165701+cgprahalad@users.noreply.github.com>
2023-12-29 11:27:34 +0000

    modified files

--- Start/Ch1/basicdata.py
+++ Start/Ch1/basicdata.py
@@ -9,10 +9,19 @@ with open("../../sample-weather-history.json", "r") as weatherfile:
     weatherdata = json.load(weatherfile)
 
 # TODO: What was the warmest day in the data set?
-
-
+maxTemp = -10
+for i in weatherdata:
+    maxTemp = max(maxTemp, i["tmax"])
+print(maxTemp)
 # TODO: What was the coldest day in the data set?
-
+minTemp = 1000
+for i in weatherdata:
+    minTemp = min(minTemp, i["tmin"])
+print(minTemp)
 
 # TODO: How many days had snowfall?
-
+snowdays = 0
+for i in weatherdata:
+    if i["snow"]>0:
+        snowdays+=1
+print(snowdays)
--- Start/Ch1/parsedata.py
+++ Start/Ch1/parsedata.py
@@ -1,14 +1,23 @@
 # Example file for Advanced Python: Hands On by Joe Marini
 # Load and parse a JSON data file and determine some information about it
-
+import json
+import pprint
 
 # TODO: open the sample weather data file and use the json module to load and parse it
-
+with open("../../sample-weather-history.json", "r") as weatherfile:
+    weatherdata = json.load(weatherfile)
 
 # TODO: make sure the data loaded correctly by printing the length of the dataset
-
+print(len(weatherdata))
 # TODO: let's also take a look at the first item in the data
-
+print(weatherdata[0])
 
 # TODO: How many days of data do we have for each year?
-
+dict = {}
+for i in weatherdata:
+    key = i["date"][:4]
+    if key in dict:
+        dict[key]+=1
+    else:
+        dict[key]=1
+pprint.pp(dict)
